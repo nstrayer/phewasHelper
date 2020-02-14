13c53a4cbd771c9a0fc38d243681a0ff9dca7578
Nick Strayer <nick.strayer@gmail.com>
2020-02-14 11:12:13 -0600

    Added normalize phecode function to new package.

---
+++ R/normalize_phecode.R
@@ -0,0 +1,24 @@
+#' Normalize a phecode vector to padded character
+#'
+#' Depending on if a phecode was ever converted to a number or not it can show
+#' up in a million different formats. This standardizes everything to a 6 digits
+#' number.
+#'
+#' @param codes A vector of phecodes in whatever format your heart desires. E.g.
+#'   `8.00`, `8`, `0008.0`, ...
+#'
+#' @return A vector of 0-padded phecodes.
+#' @export
+#'
+#' @examples
+#' normalize_phecode(c(8, 8.1, 9.2))
+normalize_phecode <- function(codes){
+
+  stringr::str_pad(
+    sprintf('%3.2f', as.numeric(codes)),
+    width = 6,
+    side = "left",
+    pad = "0"
+  )
+
+}
