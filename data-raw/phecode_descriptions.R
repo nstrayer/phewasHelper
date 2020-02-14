6d90d49862f1aa321a4ad31a3f2a052deae8c0cc
Nick Strayer <nick.strayer@gmail.com>
2020-02-14 13:50:13 -0600

    Updated phecode descriptions dataset to recode the "NULL" category as "other"

--- data-raw/phecode_descriptions.R
+++ data-raw/phecode_descriptions.R
@@ -1,14 +1,15 @@
 # Phecode 1.2 descriptions were downloaded from https://phewascatalog.org/files/phecode_definitions1.2.csv.zip
 # on 02-14-2020.
 
-phecodes <- dplyr::select(readr::read_csv('data-raw/phecode_definitions1.2.csv'),
-                          phecode,
-                          description = phenotype,
-                          category,
-                          category_number)
+library(dplyr)
 
-phecode_descriptions <- dplyr::mutate(phecodes,
-                                      phecode = normalize_phecode(phecode),
-                                      category_number = as.integer(category_number))
+phecode_descriptions <- readr::read_csv('data-raw/phecode_definitions1.2.csv') %>%
+  select(phecode,
+         description = phenotype,
+         category,
+         category_number) %>%
+   mutate(phecode = normalize_phecode(phecode),
+          category_number = as.integer(category_number),
+          category = ifelse(category == "NULL", 'other', category))
 
 usethis::use_data(phecode_descriptions)
