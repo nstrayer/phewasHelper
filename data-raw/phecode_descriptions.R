aae3e8f33bdcc9a32bb9931e506d63b52a8477e8
Nick Strayer <nick.strayer@gmail.com>
2020-02-14 14:37:32 -0600

    Added phecode index to phecode descriptions dataframe for ordered plotting etc.

--- data-raw/phecode_descriptions.R
+++ data-raw/phecode_descriptions.R
@@ -3,7 +3,6 @@
 
 library(dplyr)
 
-
 phecode_descriptions <- readr::read_csv('data-raw/phecode_definitions1.2.csv') %>%
   select(phecode,
          description = phenotype,
@@ -12,6 +11,10 @@ phecode_descriptions <- readr::read_csv('data-raw/phecode_definitions1.2.csv') %
    mutate(phecode = normalize_phecode(phecode),
           category_number = as.integer(category_number),
           category_number = ifelse(category_number == 0, 19, category_number),
-          category = ifelse(category == "NULL", 'other', category))
+          category = ifelse(category == "NULL", 'other', category)) %>%
+    arrange(category_number,
+            as.numeric(phecode)) %>%
+    mutate(phecode_index = row_number())
+
 
 usethis::use_data(phecode_descriptions, overwrite = TRUE)
